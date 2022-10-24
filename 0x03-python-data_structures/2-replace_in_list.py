d9f8d7f02867d8749d298e7b81abd7f4b2d65645
Ameermu <ameermuhd01234@gmail.com>
2022-10-24 20:54:46 +0000

    all mandatory and advanced tasks

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,6 @@
+
+0xce_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    my_list[idx] = element
+    return my_list03. Python - Data Structures: Lists, Tuples
